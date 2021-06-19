1dcb3a423e595a52b70b8c7a7fdd07b08578ee3d
Reinier Hernández <sasuke.reinier@gmail.com>
2021-06-19 15:53:51 -0400

    agregando modulo de departamento

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AreasModule } from './areas/areas.module';
 import { typeOrmConfig } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
+import { DepartmentsModule } from './departments/departments.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), AreasModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    AreasModule,
+    AuthModule,
+    DepartmentsModule,
+  ],
 })
 export class AppModule {}
