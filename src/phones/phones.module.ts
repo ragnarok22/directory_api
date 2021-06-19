eec0d206779668da2a725bfc9f575bdcec54885d
Reinier Hernández <sasuke.reinier@gmail.com>
2021-06-19 17:54:00 -0400

    arreglando problemas de importacion

--- src/phones/phones.module.ts
+++ src/phones/phones.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PhonesController } from './phones.controller';
 import { PhonesService } from './phones.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PhoneRepository } from './phone.repository';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule, TypeOrmModule.forFeature([PhoneRepository])],
   controllers: [PhonesController],
-  providers: [PhonesService]
+  providers: [PhonesService],
 })
 export class PhonesModule {}
