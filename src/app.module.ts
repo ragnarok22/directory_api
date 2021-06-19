b22c426a306b9ebe14756564cd1ebc8563ac59a6
Reinier Hernández <sasuke.reinier@gmail.com>
2021-06-19 13:53:39 -0400

    iniciando modulo de autenticacion

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AreasModule } from './areas/areas.module';
 import { typeOrmConfig } from './config/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), AreasModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), AreasModule, AuthModule],
 })
 export class AppModule {}
